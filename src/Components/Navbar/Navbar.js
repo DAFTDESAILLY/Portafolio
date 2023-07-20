ed769c3a62b7a0b653621917982801858833d602
DAFTDESAILLY <desita_gsheko@hotmail.com>
2023-07-20 02:19:13 -0600

    primer commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as S from "./Navbar.styles";
+
+function Navbar() {
+  return  <S.ContainerNavbar>
+primer container 
+  </S.ContainerNavbar>;
+  
+};
+
+export default Navbar
