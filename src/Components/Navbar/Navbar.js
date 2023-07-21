6aa0c868e37e876b708b7f75b0acc8460de848f4
DAFTDESAILLY <desita_gsheko@hotmail.com>
2023-07-21 02:27:48 -0600

    feet menu burguer y vista movil

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import * as S from "./Navbar.styles";
+import React, { useState } from 'react';
+import * as S from './Navbar.styles';
 
 function Navbar() {
-  return  <S.ContainerNavbar>
-primer container 
-  </S.ContainerNavbar>;
-  
+  const [showMenuBurguer, setShowMenuBurguer] = useState(false);
+
+  return (
+    <S.ContainerNavbar>
+      <S.ContainerLogo>
+        <S.ImageLogo src="/img/desailly-logo.png" alt="de-logo" />
+        <div>
+          <h1>Daniel Desailly</h1>
+          <h2>Frontend Dev</h2>
+        </div>
+      </S.ContainerLogo>
+
+      <S.ContainerBurger onClick={() => setShowMenuBurguer(!showMenuBurguer)}>
+        <S.BurgerMenu />
+        <S.BurgerMenu />
+        <S.BurgerMenu />
+        
+      </S.ContainerBurger>
+
+      <S.ContainerItems showMenuBurguer={showMenuBurguer}>
+        <p>About me</p>
+        <p>Projects</p>
+        <S.Button>Contact me</S.Button>
+      </S.ContainerItems>
+    </S.ContainerNavbar>
+  );
 };
 
-export default Navbar
+export default Navbar;
